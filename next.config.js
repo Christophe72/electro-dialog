d9f71c56b188127a53ce551a25449ed7eb90ce40
= <christophe.seyler@hotmail.com>
2025-08-04 19:07:12 +0200

    first commit - Application RGIE avec interface Next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['example.com'], // Remplacez par les domaines autorisés pour les images
+    domains: ["example.com"], // Remplacez par les domaines autorisés pour les images
   },
   // Ajoutez d'autres configurations spécifiques si nécessaire
 };
